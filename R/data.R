0ac3bd6cacfbe992328ec1a13924b44e53a42fa9
Tom Hull <tom.hull@cefas.gov.uk>
2022-11-07 14:17:41 +0000

    include the actual code Tom

---
+++ R/data.R
@@ -0,0 +1,30 @@
+# rbr_event_codes = data.table::fread("data/rbr_event_codes.csv", encoding = "UTF-8")[,.(Event, Description)] # TODO embed in package
+# rbr_error_codes = data.table::fread("data/rbr_error_codes.csv", encoding = "UTF-8")[,.(Error, Description)]
+# rbr_channels = data.table::fread("data/rbr_channels.csv", encoding = "UTF-8")
+# usethis::use_data(rbr_event_codes, overwrite = T)
+# usethis::use_data(rbr_error_codes, overwrite = T)
+# usethis::use_data(rbr_channels, overwrite = T)
+
+#' RBR logger3 command set - channel types
+#'
+#' A list of the supported channel types by the RBR logger3 firmware, and a description of what those channels are.
+#' Used by `read.rsk`
+#'
+#' @source <https://docs.rbr-global.com/display/L3DOCpublicvA/Supported+Channel+Types>
+"rbr_channels"
+
+#' RBR logger3 command set - error codes
+#'
+#' A list of the supported error codes by the RBR logger3 firmware, and a description of what those errors are.
+#' Used by `read.rsk`
+#'
+#' @source <https://docs.rbr-global.com/L3commandreference/format-of-stored-data/easyparse-calbin00-format/sample-data-easyparse-format>
+"rbr_error_codes"
+
+#' RBR logger3 command set - event codes
+#'
+#' A list of the supported event codes by the RBR logger3 firmware, and a description of what those events are.
+#' Used by `read.rsk`
+#'
+#' @source <https://docs.rbr-global.com/L3commandreference/format-of-stored-data/easyparse-calbin00-format/easyparse-format-events-markers>
+"rbr_event_codes"
---
+++ R/rsk.R
@@ -0,0 +1,105 @@
+options(digits.secs = 3) # RBR instruments have sub-second time resolution
+
+#' Read RBR Profiler data
+#'
+#' @param filename path to the .rsk file
+#'
+#' @import data.table
+#' @return cefasRSK object
+#' @export
+#'
+read.rsk <- function(filename){
+  con <- DBI::dbConnect(RSQLite::SQLite(), dbname = filename)
+
+  dbInfo <- RSQLite::dbReadTable(con, "dbInfo")
+  if(dbInfo$type != "EPdesktop"){stop("only tested with EPdesktop RSK files")}
+
+  events = data.table::setDT(DBI::dbReadTable(con, "events"))
+  events = merge(events, rbr_event_codes, by.x = "type", by.y = "Event")[order(tstamp)]
+
+  errors = setDT(DBI::dbReadTable(con, "errors"))
+  errors = merge(errors, rbr_error_codes, by.x = "type", by.y = "Error")
+  errors[, channelName := paste0("channel", formatC(channelOrder, 1, format = "d", flag = "0"))]
+
+  channels = setDT(DBI::dbReadTable(con, "channels"))
+  channels = merge.data.table(channels, rbr_channels[,.(Type, Description)], by.x = "shortName", by.y = "Type")
+  channels[, channelName := paste0("channel", formatC(channelID, 1, format = "d", flag = "0"))]
+
+  region_query = DBI::dbSendQuery(con, "SELECT * from region LEFT JOIN regionGeoData ON region.regionID = regionGeoData.regionID")
+  regions = data.table::setDT(DBI::dbFetch(region_query))
+  DBI::dbClearResult(region_query)
+
+  sample_period = RSQLite::dbReadTable(con, "continuous")$samplingPeriod / 1000
+
+  fields = DBI::dbListFields(con, "data") # data names
+  fields = fields[!grepl("tstamp", fields)]
+  sql_fields = paste(c("tstamp", fields), collapse = ",")
+  sql_fields = paste("SELECT", sql_fields, "FROM data")
+  data_query = DBI::dbSendQuery(con, paste(sql_fields,  ";"))
+  data = data.table::setDT(DBI::dbFetch(data_query))
+  DBI::dbClearResult(data_query)
+  data[, dateTime := as.POSIXct(tstamp/1000, origin = "1970-01-01", tz = "UTC")]
+  data.table::setnames(data, channels$channelName, channels$shortName, skip_absent = T)
+
+  ret = list() # Initialise return list containing data and metadata
+  ret[["rsk_version"]] = dbInfo
+  ret[["regions"]] = regions
+  ret[["events"]] = events
+  ret[["channels"]] = channels[order(channelID), -c("feModuleType", "feModuleVersion", "longName", "channelName")]
+  ret[["data"]] = data
+  class(ret) = "cefasRSK"
+
+  DBI::dbDisconnect(con)
+  return(ret)
+}
+
+#' Process the activation times from a RBR instrument
+#'
+#' @param rsk an object of class cefasRSK
+#' @param min_length_activation how short a activation can be in seconds
+#'
+#' @return cefasRSK object
+#' @import data.table
+#' @export
+#'
+rsk.activations <- function(rsk, min_length_activation = 120){
+  if(dbInfo$type != "EPdesktop"){stop("only tested with EPdesktop RSK files")}
+  events = rsk[["events"]]
+  data = rsk[["data"]]
+  # ---- mark activations
+  # 24 is start, 25 is pause
+  data = merge(data, events[type %in% c(24),.(tstamp, run = (type - 23))], by = "tstamp", all = T) # run is 1 for each activation
+  data[is.na(run), run := 0]
+  data[, run := cumsum(run)]
+  data[, n := .N, by = run] # how many samples per run
+  data = data[n > min_length_activation/sample_period] # get rid of short activations
+  data = na.omit(data)
+  data[, "run" := .GRP, by = run] # renumber runs
+  # data[, n := NULL]
+  ret[["activations"]] = data[,.(startTime = min(dateTime), duration = max(dateTime) - min(dateTime)), by = run]
+  return(rsk)
+}
+
+#' Add profile label and GPS metadata to data
+#'
+#' @param rsk an object of class cefasRSK
+#'
+#' @import data.table
+#' @return cefasRSK object
+#'
+rsk.regions <- function(rsk){
+  if(dbInfo$type != "EPdesktop"){stop("only tested with EPdesktop RSK files")}
+  # Apply Profiles
+  for(ID in rsk$regions[type == "PROFILE"]$regionID){
+    region = rsk$regions[regionID == ID]
+    rsk$data[tstamp %between% c(region$tstamp1, region$tstamp2),
+             profile := region$label]
+  }
+  # Apply GPS regions
+  for(ID in rsk$regions[type == "GPS"]$regionID){
+    region = rsk$regions[regionID == ID]
+    rsk$data[tstamp %between% c(region$tstamp1, region$tstamp2),
+             c("latitude", "longitude") := list(region$latitude, region$longitude)]
+  }
+  return(rsk)
+}
